62afa498c0b55d1cc810676064c83348e49dc42d
Brainyranger <122823004+Brainyranger@users.noreply.github.com>
2025-03-29 20:21:37 +0000

    update

--- script/delete_all.sh
+++ script/delete_all.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# 🚮 Suppression des ressources Kubernetes
 echo "🗑 Suppression des ressources Kubernetes..."
 kubectl delete -f k8s/
 
-echo "🧹 Suppression de l’image Docker locale..."
-docker rmi mon-dockerhub/redis:latest -f
+# 🧹 Suppression des images Docker locales
+echo "🧹 Suppression des images Docker locales..."
+
+# Suppression de l'image Docker Backend Node.js
+docker rmi redis-nodejs-backend:latest -f
+
+# Suppression de l'image Docker Frontend React
+docker rmi redis-nodejs-frontend:latest -f
+
+# Suppression de l'image officielle Redis
+docker rmi redis:latest -f
 
 echo "✅ Suppression terminée !"
